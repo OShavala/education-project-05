44c224f1d75d50e8093a6cf050152bd4ecbee81f
OShavala <olena.shvl@gmail.com>
2025-07-09 09:33:29 +0300

    fourth fixation

--- example-01.py
+++ example-01.py
@@ -1,1 +1,6 @@
+def cube_numbers(numbers):
+    return [x ** 3 for x in numbers]
 
+numbers = list(map(int, input("Введіть числа через пробіл: ").split()))
+cubes = cube_numbers(numbers)
+print(cubes)
--- example-02.py
+++ example-02.py
@@ -1,1 +1,10 @@
+def short_long_words(words_list):
+    long_word = max(words_list, key=len)
+    short_word = min(words_list, key=len)
+    return short_word, long_word
+
+# Приклад використання
+words = ["apple", "orange"]
+result = short_long_words(words)
+print(result)  
 
